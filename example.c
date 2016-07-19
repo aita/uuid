226478c64b29c796b78fa677349089c05137c542
Ryo <ryoait@gmail.com>
2016-07-20 00:50:16 +0900

    use uuid_string_t in example.c

--- example.c
+++ example.c
@@ -6,12 +6,12 @@ main()
 {
     int i;
     uuid_t uu;
-    char buf[37];
+    uuid_string_t out;
 
     for (i = 0; i < 10; i++) {
         uuid_generate(uu);
-        uuid_unparse(uu, buf);
-        printf("%s\n", buf);
+        uuid_unparse(uu, out);
+        printf("%s\n", out);
     }
 
     return 0;
